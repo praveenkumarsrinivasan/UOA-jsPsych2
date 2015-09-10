b7700e0aa607108c38186c43cff39b523793fb8c
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2015-09-10 05:42:08 +0000

    complete hit condition bug

---
+++ scripts/dump_json_output.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 praveenkumarsrinivasan <praveen.sxi@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Script to dump the JSPsych MySQL JsonOutput ot Terminal
+"""
+
+def dump_json_output():
+
